304c874841bee8847e3253ef7098cc817550f6bf
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-14 00:26:24 +0100

    Implement user and role repositories

---
+++ book-network/src/main/java/com/soumaya/book_network/role/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.soumaya.book_network.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String role);
+
+}
