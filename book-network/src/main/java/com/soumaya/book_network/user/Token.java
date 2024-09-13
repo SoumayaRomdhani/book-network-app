9ba733438e968ac1eb68234dcb3a22c8fb933dd0
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-14 00:41:39 +0100

    Create the Token entity and repository

---
+++ book-network/src/main/java/com/soumaya/book_network/user/Token.java
@@ -0,0 +1,31 @@
+package com.soumaya.book_network.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private  String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
+
+
