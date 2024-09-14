f46b01d2e2cc1a4dc98ca552d884c83392461e3e
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-14 17:31:19 +0100

    implement the register method

--- book-network/src/main/java/com/soumaya/book_network/auth/AuthenticationService.java
+++ book-network/src/main/java/com/soumaya/book_network/auth/AuthenticationService.java
@@ -1,10 +1,77 @@
 package com.soumaya.book_network.auth;
 
+import com.soumaya.book_network.role.RoleRepository;
+import com.soumaya.book_network.user.Token;
+import com.soumaya.book_network.user.TokenRepository;
+import com.soumaya.book_network.user.User;
+import com.soumaya.book_network.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
     public void register(RegistrationRequest request) {
+
+        var userRole = roleRepository.findByName("USER")
+                // todo - better exception handling
+                .orElseThrow(() -> new IllegalStateException("ROLE USER was not initiated"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
     }
+
+    private Object generateAndSaveActivationToken(User user) {
+        // Generate a token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+
+        tokenRepository.save(token);
+
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+
+        SecureRandom secureRandom = new SecureRandom();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+
+        return codeBuilder.toString();
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+    }
+
+
 }
