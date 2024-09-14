6e3c0f203cfef54c3329683a1ec434e96bca86d8
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-14 16:31:30 +0100

    create the authentication controller

---
+++ book-network/src/main/java/com/soumaya/book_network/auth/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.soumaya.book_network.auth;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationRequest request
+    ) {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+    }
+
+}
---
+++ book-network/src/main/java/com/soumaya/book_network/auth/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.soumaya.book_network.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    public void register(RegistrationRequest request) {
+    }
+}
