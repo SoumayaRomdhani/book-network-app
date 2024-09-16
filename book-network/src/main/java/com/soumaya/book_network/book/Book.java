fd670552da36645c3a5090ead475df341fadc609
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-16 20:50:47 +0100

    Use inheritance and refactor the code

--- book-network/src/main/java/com/soumaya/book_network/book/Book.java
+++ book-network/src/main/java/com/soumaya/book_network/book/Book.java
@@ -1,6 +1,12 @@
 package com.soumaya.book_network.book;
 
+import com.soumaya.book_network.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -9,14 +15,15 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Book {
+public class Book extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
+
     private String title;
     private String authorName;
     private String isbn;
@@ -25,20 +32,6 @@ public class Book {
     private boolean archived;
     private boolean shareable;
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
 
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-
-    @CreatedBy
-    @Column(nullable = false,updatable = false)
-    private Integer createdBy;
-
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 
 }
--- book-network/src/main/java/com/soumaya/book_network/feedback/Feedback.java
+++ book-network/src/main/java/com/soumaya/book_network/feedback/Feedback.java
@@ -1,7 +1,13 @@
 package com.soumaya.book_network.feedback;
 
 
+import com.soumaya.book_network.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -9,29 +15,16 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Feedback {
-    @Id
-    @GeneratedValue
-    private Integer id;
+public class Feedback extends BaseEntity {
+
     private Double note;//1-5 stars
     private String comment ;
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
 
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-
-    @CreatedBy
-    @Column(nullable = false,updatable = false)
-    private Integer createdBy;
-
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 }
