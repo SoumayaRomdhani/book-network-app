b3f4195b04a34a8da4af53468e79e82d7cf09fb0
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-19 18:16:09 +0100

    creation of app.module.ts

---
+++ book-network-ui/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+  providers: [
+    HttpClient
+    ],
+  bootstrap: [AppComponent]
+})
+
+
+export class AppModule { }
