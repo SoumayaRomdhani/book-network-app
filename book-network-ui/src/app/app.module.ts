fcaad56cb72362a748cf203dca90e0278320cd92
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-20 18:09:44 +0100

    implement the register page

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- book-network-ui/src/app/pages/register/register.component.ts
+++ book-network-ui/src/app/pages/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {RegistrationRequest} from '../../services/models/registration-request';
+import {AuthenticationService} from '../../services/services/authentication.service';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  registerRequest: RegistrationRequest = {email: '', firstname: '', lastname: '', password: ''};
+  errorMsg: Array<string> = [];
+
+   constructor(
+      private router: Router,
+      private authService: AuthenticationService
+    ) {
+    }
+
+  register(){
+    this.errorMsg = [];
+        this.authService.register({
+          body: this.registerRequest
+        })
+          .subscribe({
+            next: () => {
+              this.router.navigate(['activate-account']);
+            },
+            error: (err) => {
+              this.errorMsg = err.error.validationErrors;
+            }
+          });
+    }
+
+  login(){
+    this.router.navigate(['login']);
+    }
+
+
 
 }
