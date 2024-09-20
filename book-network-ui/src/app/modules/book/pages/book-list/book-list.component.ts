a92800302ffa26e21a82070fb6be4a499b920893
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-20 23:19:10 +0100

    implement the book list page (part 1)

--- book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {BookService} from '../../../../services/services/book.service';
+import {Router} from '@angular/router';
+import {PageResponseBookResponse} from '../../../../services/models/page-response-book-response';
+import {BookResponse} from '../../../../services/models/book-response';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit{
+  bookResponse: PageResponseBookResponse = {};
+  page = 0;
+  size = 5;
+
+
+  constructor(
+      private bookService: BookService,
+      private router: Router
+  ) {
+    }
+
+  ngOnInit(): void {
+      this.findAllBooks();
+    }
+
+  private findAllBooks() {
+      this.bookService.findAllBooks({
+        page: this.page,
+        size: this.size
+      })
+        .subscribe({
+          next: (books) => {
+            this.bookResponse = books;
+
+          }
+        });
+    }
+
+
+
+
 
 }
