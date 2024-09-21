3011477178d78bb1a8f6a7ee1b0a51afba68c785
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-21 20:28:15 +0100

    implement the create book method

--- book-network-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
+++ book-network-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookRequest} from '../../../../services/models/book-request';
+import {BookService} from '../../../../services/services/book.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-manage-book',
@@ -6,5 +9,60 @@ import { Component } from '@angular/core';
   styleUrl: './manage-book.component.scss'
 })
 export class ManageBookComponent {
+
+  errorMsg: Array<string> = [];
+  selectedPicture: string | undefined;
+  selectedBookCover: any;
+  bookRequest: BookRequest = {
+      authorName: '',
+      isbn: '',
+      synopsis: '',
+      title: ''
+    };
+
+  constructor(
+      private bookService: BookService,
+      private router: Router,
+      private activatedRoute: ActivatedRoute
+    ) {
+    }
+
+
+  onFileSelected(event: any) {
+      this.selectedBookCover = event.target.files[0];
+            console.log(this.selectedBookCover);
+
+            if (this.selectedBookCover) {
+
+              const reader = new FileReader();
+              reader.onload = () => {
+                this.selectedPicture = reader.result as string;
+              };
+              reader.readAsDataURL(this.selectedBookCover);
+            }
+    }
+
+  saveBook() {
+      this.bookService.saveBook({
+        body: this.bookRequest
+      }).subscribe({
+        next: (bookId) => {
+          this.bookService.uploadBookCoverPicture({
+            'book-id': bookId,
+            body: {
+              file: this.selectedBookCover
+            }
+          }).subscribe({
+            next: () => {
+              this.router.navigate(['/books/my-books']);
+            }
+          });
+        },
+        error: (err) => {
+          console.log(err.error);
+          this.errorMsg = err.error.validationErrors;
+        }
+      });
+    }
 
 }
