e702792f2c9ab919787f4ed33a82ac3660c121f3
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-21 01:16:07 +0100

    add interceptor

---
+++ book-network-ui/src/app/services/interceptor/http-token.interceptor.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { TokenService } from '../token/token.Service';
+
+
+
+@Injectable()
+export class HttpTokenInterceptor implements HttpInterceptor {
+
+  constructor(
+    private tokenService: TokenService
+  ) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = this.tokenService.token;
+    if (token) {
+      const authReq = request.clone({
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        })
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(request);
+  }
+}
