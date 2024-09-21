cab1f43668897b75bcb52bb8a812adeb38f07da8
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-21 02:18:21 +0100

    inject the jwt token using an http interceptor

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -8,6 +8,7 @@ import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import {CodeInputModule} from 'angular-code-input';
+import {HttpTokenInterceptor} from './services/interceptor/http-token.interceptor';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,11 @@ import {CodeInputModule} from 'angular-code-input';
   ],
   providers: [
     HttpClient,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: HttpTokenInterceptor,
+          multi: true
+        },
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
--- book-network-ui/src/app/services/interceptor/http-token.interceptor.ts
+++ book-network-ui/src/app/services/interceptor/http-token.interceptor.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import { TokenService } from '../token/token.Service';
+import { TokenService } from '../token/token.service';
 
 
 
