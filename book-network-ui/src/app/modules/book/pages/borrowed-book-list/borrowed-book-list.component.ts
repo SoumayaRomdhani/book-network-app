8c80474a0f42decf49c6c65bf8e77dff5e72a870
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-22 00:04:41 +0100

    add borrowed-book-list component

---
+++ book-network-ui/src/app/modules/book/pages/borrowed-book-list/borrowed-book-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-borrowed-book-list',
+  templateUrl: './borrowed-book-list.component.html',
+  styleUrl: './borrowed-book-list.component.scss'
+})
+export class BorrowedBookListComponent {
+
+}
