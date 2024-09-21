f42993ad31f3310c3bb5731288ec274d141c4633
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-21 16:44:03 +0100

    implement the rating component

--- book-network-ui/src/app/modules/book/components/rating/rating.component.ts
+++ book-network-ui/src/app/modules/book/components/rating/rating.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
 
 @Component({
   selector: 'app-rating',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './rating.component.scss'
 })
 export class RatingComponent {
+
+  @Input() rating: number = 0;
+  maxRating: number = 5;
+
+  get fullStars(): number {
+      return Math.floor(this.rating);
+    }
+
+  get hasHalfStar(): boolean {
+      return this.rating % 1 !== 0;
+    }
+
+  get emptyStars(): number {
+      return this.maxRating - Math.ceil(this.rating);
+    }
 
 }
