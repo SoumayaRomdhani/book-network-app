8bca168932177aad2c63f69007c9f3c229bc7eed
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-22 01:19:56 +0100

    Implement the return borrowed book page

--- book-network-ui/src/app/modules/book/book.module.ts
+++ book-network-ui/src/app/modules/book/book.module.ts
@@ -10,6 +10,8 @@ import { RatingComponent } from './components/rating/rating.component';
 import { MyBooksComponent } from './pages/my-books/my-books.component';
 import { ManageBookComponent } from './pages/manage-book/manage-book.component';
 import {FormsModule} from '@angular/forms';
+import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
+import { ReturnBooksComponent } from './pages/return-books/return-books.component';
 
 
 @NgModule({
@@ -20,7 +22,9 @@ import {FormsModule} from '@angular/forms';
     BookCardComponent,
     RatingComponent,
     MyBooksComponent,
-    ManageBookComponent
+    ManageBookComponent,
+    BorrowedBookListComponent,
+    ReturnBooksComponent
   ],
   imports: [
     CommonModule,
