421f516850f4c5cfe44fd2996a2110f384589697
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-22 02:33:49 +0100

    Secure the routes with Auth Guard

--- book-network-ui/src/app/services/guard/auth.guard.ts
+++ book-network-ui/src/app/services/guard/auth.guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import {TokenService} from '../token/token.service';
+import {inject} from '@angular/core';
+//import {KeycloakService} from '../keycloak/keycloak.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
+  const tokenService = inject(TokenService);
+  const router = inject(Router);
+  if (tokenService.isTokenNotValid()) {
+    router.navigate(['login']);
+    return false;
+  }
   return true;
 };
--- book-network-ui/src/app/services/token/token.service.ts
+++ book-network-ui/src/app/services/token/token.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,26 @@ export class TokenService {
 
   get token() {
       return localStorage.getItem('token') as string;
+    }
+
+  isTokenValid() {
+      const token = this.token;
+      if (!token) {
+        return false;
+      }
+      // decode the token
+      const jwtHelper = new JwtHelperService();
+      // check expiry date
+      const isTokenExpired = jwtHelper.isTokenExpired(token);
+      if (isTokenExpired) {
+        localStorage.clear();
+        return false;
+      }
+      return true;
+    }
+
+  isTokenNotValid() {
+      return !this.isTokenValid();
     }
 
 }
