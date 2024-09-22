1efc40f631e72c2a92940dffd29c0ef1ab826517
Soumaya Romdhani <soumayaromdhani2003@gmail.com>
2024-09-22 01:19:09 +0100

    add return-books component

---
+++ book-network-ui/src/app/modules/book/pages/return-books/return-books.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-return-books',
+  templateUrl: './return-books.component.html',
+  styleUrl: './return-books.component.scss'
+})
+export class ReturnBooksComponent {
+
+}
